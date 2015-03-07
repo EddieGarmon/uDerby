bc48d36a6fbeeefb98f737bee4a15740874319ec
Eddie Garmon <eddie_garmon@hotmail.com>
2015-03-07 04:32:48 -0500

    Migrate Source from uScoober

---
+++ Source/uDerby/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using uScoober.Threading;
+
+namespace uDerby
+{
+    public static class EntryPoint
+    {
+        public static void Main()
+        {
+            //TaskScheduler.UnobservedExceptionHandler += exception => Debug.Print(exception.ToString());
+
+            // warmup task engine
+            Task.Run(() => { })
+                .Wait();
+
+            //wow, an IoT Pinewood Derby car? add some sensors...
+            var car = new Car();
+            car.Start();
+        }
+    }
+}
